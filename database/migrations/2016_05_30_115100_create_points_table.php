d4956eeeae940e0b14796daa34646538a8416080
lyxuansang91 <lyxuansang1991@gmail.com>
2016-06-02 20:52:30 +0700

    Add SemesterSubjectLevel and point

--- database/migrations/2016_05_30_115100_create_points_table.php
+++ database/migrations/2016_05_30_115100_create_points_table.php
@@ -16,17 +16,17 @@ class CreatePointsTable extends Migration
             $table->increments('id');
             $table->integer('student_id');
             $table->integer('semester_subject_level_id');
-            $table->double('mark_m1', 5, 1);
-            $table->double('mark_m2', 5, 1);
-            $table->double('mark_m3', 5, 1);
-            $table->double('mark_m4', 5, 1);
-            $table->double('mark_15_1', 5, 1);
-            $table->double('mark_15_2', 5, 1);
-            $table->double('mark_15_3', 5, 1);
-            $table->double('mark_45_1', 5, 1);
-            $table->double('mark_45_2', 5, 1);
-            $table->double('mark_last', 5, 1);
-            $table->double('mark_avg', 5, 1);
+            $table->double('mark_m1', 5, 1)->nullable();
+            $table->double('mark_m2', 5, 1)->nullable();
+            $table->double('mark_m3', 5, 1)->nullable();
+            $table->double('mark_m4', 5, 1)->nullable();
+            $table->double('mark_15_1', 5, 1)->nullable();
+            $table->double('mark_15_2', 5, 1)->nullable();
+            $table->double('mark_15_3', 5, 1)->nullable();
+            $table->double('mark_45_1', 5, 1)->nullable();
+            $table->double('mark_45_2', 5, 1)->nullable();
+            $table->double('mark_last', 5, 1)->nullable();
+            $table->double('mark_avg', 5, 1)->nullable();
             $table->timestamps();
         });
     }
--- database/seeds/StudentTableSeeder.php
+++ database/seeds/StudentTableSeeder.php
@@ -22,7 +22,74 @@ class StudentTableSeeder extends Seeder
                 'level_id'=> 1,
                 'created_at'=> \Carbon\Carbon::now(),
                 'updated_at' => \Carbon\Carbon::now()
-            ]
+            ],
+            [
+               'name'=> 'Lý Xuân Sang 2',
+               'gender'=> 0,
+               'birthday'=> '1991-02-04',
+               'address'=> 'Hà Nội',
+               'phone'=> '09999999',
+               'level_id'=> 1,
+               'created_at'=> \Carbon\Carbon::now(),
+               'updated_at' => \Carbon\Carbon::now()
+           ],
+           [
+              'name'=> 'Lý Xuân Sang 3',
+              'gender'=> 0,
+              'birthday'=> '1991-02-04',
+              'address'=> 'Hà Nội',
+              'phone'=> '09999999',
+              'level_id'=> 1,
+              'created_at'=> \Carbon\Carbon::now(),
+              'updated_at' => \Carbon\Carbon::now()
+          ],
+          [
+             'name'=> 'Lý Xuân Sang 4',
+             'gender'=> 0,
+             'birthday'=> '1991-02-04',
+             'address'=> 'Hà Nội',
+             'phone'=> '09999999',
+             'level_id'=> 1,
+             'created_at'=> \Carbon\Carbon::now(),
+             'updated_at' => \Carbon\Carbon::now()
+         ]
+         , [
+            'name'=> 'Lý Xuân Sang 5',
+            'gender'=> 0,
+            'birthday'=> '1991-02-04',
+            'address'=> 'Hà Nội',
+            'phone'=> '09999999',
+            'level_id'=> 1,
+            'created_at'=> \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]
+        , [
+           'name'=> 'Lý Xuân Sang 6',
+           'gender'=> 0,
+           'birthday'=> '1991-02-04',
+           'address'=> 'Hà Nội',
+           'phone'=> '09999999',
+           'level_id'=> 1,
+           'created_at'=> \Carbon\Carbon::now(),
+           'updated_at' => \Carbon\Carbon::now()
+        ]
+
         ]);
+
+        DB::table('semester_subject_levels')->insert([
+            'semester_id' => 1,
+            'subject_id' => 1,
+            'level_id' => 1,
+            'teacher_id'=> 1,
+            'created_at'=> \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+        $students = \App\Models\Student::where('level_id', 1)->get();
+        foreach($students as $student) {
+            $point = new \App\Models\Point();
+            $point->semester_subject_level_id = 1;
+            $point->student_id = $student->id;
+            $point->save();
+        }
     }
 }
